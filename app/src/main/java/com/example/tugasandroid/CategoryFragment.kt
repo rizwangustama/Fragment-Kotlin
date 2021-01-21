579468a17df24c04b06105499b40ba3200724a40
rizwangustama <rizonegustama@gmail.com>
2021-01-21 15:58:35 +0700

    Fragment

---
+++ app/src/main/java/com/example/tugasandroid/CategoryFragment.kt
@@ -0,0 +1,16 @@
+package com.example.tugasandroid
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class CategoryFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/tugasandroid/DetailCategoryFragment.kt
@@ -0,0 +1,18 @@
+package com.example.tugasandroid
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class DetailCategoryFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_category, container, false)
+    }
+}
